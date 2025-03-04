5d6176044c424e6192a5fac5f9a2c530fa9d18e3
argonaultes <gael@argonaultes.fr>
2025-03-04 12:01:21 +0100

    opened closed

---
+++ solid/o_good.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'Person(name={self.name})'
+
+
+class PersonStorage(ABC):
+    @abstractmethod
+    def save(self, person):
+        pass
+
+class PersonDB(PersonStorage):
+    def save(self, person):
+        print(f'Save the {person} to database')
+
+class PersonJson(PersonStorage):
+    def save(self, person):
+        print(f'Save the {person} to a JSON file')
+
+class PersonXML(PersonStorage):
+    def save(self, person):
+        pass
+
+
+if __name__ == '__main__':
+    person = Person('John Doe')
+    storage = PersonXML()
+    storage.save(person)
+    storageabc = PersonStorage()
