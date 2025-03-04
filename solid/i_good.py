0c363775cc25a23c6a08acae3f20ce711b114dc9
argonaultes <gael@argonaultes.fr>
2025-03-04 12:40:40 +0100

    uml interface segregation

---
+++ solid/i_good.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    @abstractmethod
+    def go(self):
+        pass
+
+class Flying(ABC):
+    @abstractmethod
+    def fly(self):
+        pass
+
+class Aircraft(Vehicle, Flying):
+    def go(self):
+        print("Taxiing")
+
+    def fly(self):
+        print("Flying")
+
+class Car(Vehicle):
+    def go(self):
+        print("Going")
+
+if __name__ == '__main__':
+    supercar = Car()
+    supercar.go()
+    superfly = Aircraft()
+    superfly.go()
+    superfly.fly()
