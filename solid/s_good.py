992f65881d7efde0a345d0a179d100943f0c35e5
argonaultes <gael@argonaultes.fr>
2025-03-04 11:41:37 +0100

    single resp fixed

---
+++ solid/s_good.py
@@ -0,0 +1,17 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'Person(name={self.name})'
+
+
+class PersonStorage:
+    @classmethod
+    def save(cls, person):
+        print(f'Save the {person} to the database')
+
+
+if __name__ == '__main__':
+    p = Person('John Doe')
+    PersonStorage.save(p)
