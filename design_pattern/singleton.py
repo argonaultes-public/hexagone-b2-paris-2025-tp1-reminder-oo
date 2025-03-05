136636cfb1e7965740d7973c540c53ea84971a18
argonaultes <gael@argonaultes.fr>
2025-03-05 10:07:15 +0100

    singleton v1

--- design_pattern/singleton.py
+++ design_pattern/singleton.py
@@ -1,9 +1,20 @@
 
 # S'assurer qu'une classe a une seule et unique instance et est accessible depuis n'importe où.
 
+# Solution 1
+# Utiliser 1 attribut de classe et 1 méthode de classe 
+
 class School:
+    __instance = None
     def __init__(self, name = None):
-        self.__name = name
+            self.students = []
+            self.__name = name
+
+    @classmethod
+    def get_instance(cls, name: str):
+        if cls.__instance is None:
+            cls.__instance = School(name=name)
+        return cls.__instance
 
     @property
     def name(self):
@@ -18,5 +29,10 @@ class School:
 
 if __name__ == '__main__':
     school1 = School('school1')
-    school2 = School('school1')
+    school2 = School('school2')
+    school1.students.append('student1')
+    school1.students.append('student2')
+    print('school1', school1.students)
+    print('school2', school2.students)
+    print(school1, school2)
     assert school1 == school2
